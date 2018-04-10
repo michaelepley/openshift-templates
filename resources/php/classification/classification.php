8cf8dc44e4032839cfad993a8cd368825376e51d
mepley <mepley@breccia>
2018-04-10 01:18:27 -0400

    Added php classification banner template

---
+++ resources/php/classification/classification.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+html,body {
+	background-color: #ffffff;
+	height: 100%;
+	width: 100%;
+	margin: 0; 
+	padding: 0;
+	overflow: hidden;
+	color: #000000;
+}
+
+table {
+    border-collapse: collapse;
+}
+
+tr {
+  background-color: green;
+  color: white;
+  font-family: sans;
+  font-size: 20px;
+}
+
+td {
+  text-align:center;
+  color=white;
+}
+
+table, th, td, tr {
+  border: 0px;
+  padding: 0px;
+  margin: 0px;
+}
+</style>
+</head>
+<body>
+<table width="100%" height="100%">
+	<tr height="24"><td width="100%">UNCLASSIFIED - FOR DEMO USE ONLY</td></tr>
+	<tr height="*" width="100%"><td height="100%" width="100%"><iframe height="100%" width="100%" src="index-previous.php"></iframe></td></tr>
+	<tr height="24"><td>UNCLASSIFIED - FOR DEMO USE ONLY</td></tr>
+</table>
+</body>
+</html>
